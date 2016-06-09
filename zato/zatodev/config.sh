6a3b856cebefe88554f3c53d0264a9802b2c9cfc
gage <gagerenzi@gmail.com>
2016-06-09 14:17:19 -0500

    updates to script

--- zato/zatodev/config.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#doean't quite work. need to add basic http user manually for now
-#curl -H "Content-Type: application/json" -X POST -d '{"cluster_id": 1,"name": "python dev client","is_active": true,"username": "pydev","realm": "Zato admin invoke"}' http://localhost:11223/zato/json/zato.security.basic-auth.create
--- zato/zatodev/go.sh
+++ zato/zatodev/go.sh
@@ -2,6 +2,7 @@
 $ZATODIR/zato-qs-stop.sh
 cd configs
 zato-deploy
+cd ..
 find $ZATODIR | grep pid | xargs rm
 $ZATODIR/zato-qs-start.sh
 netstat -al | grep LIST

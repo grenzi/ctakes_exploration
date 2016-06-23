bfc3ac1b33c7ec8905affce29a4f441cb03628e4
benjamin renzi <gagerenzi@gmail.com>
2016-06-23 09:48:17 -0500

    cleanup / clarification

--- zato/zatodev/deploy.sh
+++ zato/zatodev/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo ZATOPASS SHOULD BE SET IN THE ENVIRONMENT.
+admininvokepass=$(echo "SELECT `password` FROM zato.sec_base where name = 'admin.invoke' limit 1" | mysql zato -uzatoapp -pzatoapp)
+echo 'export ZATOPASS='"$admininvokepass"
 rm $ZATODIR/server1/pickup-dir/* 2>/dev/null
 find ./services -name "*.py" -not -name "__init__*" | xargs -i cp {} $ZATODIR/server1/pickup-dir/
 ls -l $ZATODIR/server1/pickup-dir

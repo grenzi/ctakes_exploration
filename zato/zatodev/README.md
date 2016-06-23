c48bec9a01bfbc9d1967b8925c715161ef3a8e87
benjamin renzi <gagerenzi@gmail.com>
2016-06-22 22:01:09 -0500

    AUTOLOADING CHANNELS. YES.

--- zato/zatodev/README.md
+++ zato/zatodev/README.md
@@ -1,4 +1,4 @@
-# Zato.io Core
+# CTE Core Codebase
 
 ## about
 blue creek uses zato.io to expose its services. This lets us scale out as
@@ -19,7 +19,6 @@ that configuration.
 - [deploy.sh](deploy.sh): should be run from Vagrant VM.
   - copies service files from the cte subfolder here to the
     vagrant deploy directory, where zato will hot-deploy the updates
-  - tails the zato server1 log file so you can see the deploy happened
 - [test.sh](test.sh): can be run from either host or guest OS. will
   probably work better on guest
   - runs zato's unit testing (apitest) against items in the [test](test) folder
@@ -33,9 +32,97 @@ that configuration.
   that said, given the [../doc/source/conf.py](../doc/source/conf.py)
 - if line endings get wonko (windows style) run ```find -type f | xargs sed -i -e 's/^M//'```
 
-## sphinx issue
-even though the conf.py lists the right source directory, I'm not seeing any autodoc
-skeleton content generated in the sphinx output. know it's got to be something simple
-I'm missing
+## why the api is structured the way it is and isn't that cool new REST format with fancy methods, etc
+todo: put explanation here, SIO, SOAP, etc. also cover adding additoinal channels later (maybe)
 
+## Service Template
+(todo - create a script to create this...)
+
+```
+from __future__ import absolute_import, division, print_function, unicode_literals
+import os.path
+from zato.server.service import Service
+from contextlib import closing
+from sql import cte
+import json
+from bunch import *
+from cteutil import AlchemyEncoder, CteServiceBase
+from sqlalchemy.ext.declarative import DeclarativeMeta
+import sys
+
+# if you want to use a sqlalchemy object
+# (see zatodev/sql/* files)
+# from sql import modelname
+
+# todo set the class name
+class RestExB(Service):
+    class SimpleIO:
+        # TODO define inputs below usingn input_optional and input_required
+        output_optional = ('data',)
+        input_optional = ('id',)
+        # input_required = ('id',)
+
+    @staticmethod
+    def get_name():
+        # TODO fill in name in the format cte.service.noun.verb
+        # verb should be one of these if applicable:
+        #   Create
+        #   Delete
+        #   Edit
+        #   GetByID
+        #   GetList
+        return 'cte.service.noun.verb'
+
+    @staticmethod
+    def get_channels():
+        # TODO fill in the information to expose this service (right now just JSON over http
+        # verb should be one of these if applicable. hypens should be removed for urls
+        #   create
+        #   delete
+        #   edit
+        #   get-by-id
+        #   get-list
+        return [{'name': u'cte.noun.get-list', 'method': u'GET', 'path': u'/api/noun/getlist',},
+                {'name': u'cte.noun.create', 'method': u'POST', 'path': u'/api/noun/create',}]
+
+    @staticmethod
+    def after_add_to_store(logger):
+        from cteutil import ChannelUtils
+        logger.info('Configuring {}'.format(RestExB.get_name()))
+        cu = ChannelUtils(logger)
+        cu.configure_service_file(os.path.realpath(__file__))
+
+    #todo - make stub handle examples for get/create/delete/etc
+    def handle(self):
+        self.initProcessing()
+        data = Bunch()
+
+        try:
+            with self.getCteSession() as session:
+                c = session.query(cte.Corpus).filter_by(id=self.input.id).first()
+                self.payload.data = AlchemyEncoder.toJsonObj(c)
+                session.commit()
+
+            self.endProcessing()
+        except Exception as e:
+            self.payload.status.code = 500
+            self.payload.status.msg = "ERR"
+            self.payload.status.error = e
+            self.payload.data = None
+
+
+# so there are two ways to automatically run the configuration
+# 1. just execute the file (original plan)
+# 2. just deploy it (how cool is that?!?
+def configure_service_on_zato():
+    sys.path.append(os.environ['ZATOEXTRAPATHS'])
+    from cteutil import ChannelUtils
+    cu = ChannelUtils()
+    cu.configure_service_file(os.path.realpath(__file__))
+
+
+if __name__ == "__main__":
+    configure_service_on_zato()
+
+```
 
--- zato/zatodev/lib/cteutil/__init__.py
+++ zato/zatodev/lib/cteutil/__init__.py
@@ -1,13 +1,34 @@
 __version__ = '1.0.0'
 VERSION = tuple(map(int, __version__.split('.')))
 
-__all__ = ('AlchemyEncoder','CteServiceBase')
+__all__ = ('AlchemyEncoder','CteServiceBase', 'ChannelUtils')
 
 import json
 from sqlalchemy.ext.declarative import DeclarativeMeta
 from zato.server.service import Service
+from zato.client import AnyServiceInvoker
+import ast
+import os.path
 from contextlib import closing
 from bunch import *
+
+
+def load_from_file(filepath, expected_class):
+    import imp
+    class_inst = None
+
+    mod_name, file_ext = os.path.splitext(os.path.split(filepath)[-1])
+
+    if file_ext.lower() == '.py':
+        py_mod = imp.load_source(mod_name, filepath)
+
+    elif file_ext.lower() == '.pyc':
+        py_mod = imp.load_compiled(mod_name, filepath)
+
+    if hasattr(py_mod, expected_class):
+        class_inst = getattr(py_mod, expected_class)()
+
+    return class_inst
 
 class AlchemyEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -31,6 +52,103 @@ class AlchemyEncoder(json.JSONEncoder):
         a = json.dumps(obj, cls=AlchemyEncoder)
         b = json.loads(a)
         return b
+
+
+class ChannelUtils:
+    def __init__(self, logger=None):
+        try:
+            self.address = 'http://localhost:11223'
+            self.path = '/zato/admin/invoke'
+            self.auth = ('admin.invoke', os.environ["ZATOPASS"])
+            self.client = AnyServiceInvoker(self.address, self.path, self.auth)
+            self.channeldict = None
+            self.loggerobj = logger
+        except KeyError:
+            print "Please set the environment variable ZATOPASS"
+            self.log('Please set the environment variable ZATOPASS')
+            sys.exit(1)
+
+    def get_channels(self):
+        self.channeldict = {}
+
+        payload = {"cluster_id": 1, "connection": "channel", "transport": "plain_http"}
+        response = self.client.invoke('zato.http-soap.get-list', payload=payload)
+
+        if response.ok:
+            d = response.data
+            for c in d:
+                self.channeldict[c[ u'name']]=c[ u'id']
+            return self.channeldict
+        else:
+            self.log('ERROR getting channel dictionary')
+            return {}
+
+    def delete_channel(self, id):
+        self.channeldict = {}
+
+        payload = {"id": id}
+        response = self.client.invoke('zato.http-soap.delete', payload=payload)
+
+        if response.ok:
+            pass
+        else:
+            print(response.details)
+            return {}
+
+    def add_channel(self, channelname, servicename, url, method, skipifexists=False):
+        if self.channeldict is None:
+            self.get_channels()
+
+        if skipifexists == False and channelname in self.channeldict:
+            self.delete_channel(id=self.channeldict[channelname])
+
+        payload = {}
+        payload[u'connection'] = u'channel'
+        payload[u'name'] = channelname
+        payload[u'cluster_id'] = 1
+        payload[u'transport'] = u'plain_http'
+        payload[u'url_path'] = url
+        payload[u'service'] = servicename
+        payload[u'method'] = method
+        payload[u'data_format'] = u'json'
+        payload[u'is_active']=True
+        payload[u'is_internal']=False
+
+        response = self.client.invoke('zato.http-soap.create', payload=payload)
+
+        if response.ok:
+            pass
+        else:
+            print(response.details)
+            return {}
+
+    def log(self, msg):
+        if self.loggerobj is not None:
+            self.loggerobj.info(msg)
+        else:
+            print msg
+
+    def configure_service_file(self, filepath):
+        source = ''
+        self.log('Processing {}'.format(filepath))
+        with open(filepath, 'r') as f:
+            source = f.read()
+        p = ast.parse(source)
+
+        classes = [node.name for node in ast.walk(p) if isinstance(node, ast.ClassDef)]
+        for classname in classes:
+            if classname != 'SimpleIO':
+                classobj = load_from_file(filepath, classname)
+
+                namemethod = getattr(classobj, 'get_name')
+                servicename = namemethod()
+
+                channelmethod = getattr(classobj, 'get_channels')
+                channels = channelmethod()
+
+                for channel in channels:
+                    self.log('Adding channel {}'.format(channel['name']))
+                    self.add_channel(channel['name'], servicename, channel['path'], channel['method'])
 
 class CteServiceBase():
     req = None
@@ -58,3 +176,4 @@ class CteServiceBase():
 
     def endProcessing(self):
         self.log_output("-----------------------output:")
+

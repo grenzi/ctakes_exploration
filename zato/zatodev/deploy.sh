d74b5e47a4ea41dce175fca310e77c82af2a2206
benjamin renzi <gagerenzi@gmail.com>
2016-06-24 12:44:10 -0500

    reworking services to new template

--- zato/zatodev/deploy.sh
+++ zato/zatodev/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-echo ZATOPASS SHOULD BE SET IN THE ENVIRONMENT.
-admininvokepass=$(echo "SELECT `password` FROM zato.sec_base where name = 'admin.invoke' limit 1" | mysql zato -uzatoapp -pzatoapp)
-echo 'export ZATOPASS='"$admininvokepass"
-rm $ZATODIR/server1/pickup-dir/* 2>/dev/null
+#MYSQLHOST=$(/sbin/ip route | awk '/default/ { print $3 }')
+#echo ZATOPASS SHOULD BE SET IN THE ENVIRONMENT.
+#admininvokepass=$(echo "SELECT \`password\` FROM zato.sec_base where name = 'admin.invoke' limit 1" | mysql zato -uzatoapp -pzatoapp -h$MYSQLHOST)
+#admininvokepass=782fd57d201a4ff39918c09f100cd3f4
+#echo 'export ZATOPASS='"$admininvokepass"
+#rm $ZATODIR/server1/pickup-dir/* 2>/dev/null
 find ./services -name "*.py" -not -name "__init__*" | xargs -i cp {} $ZATODIR/server1/pickup-dir/
 ls -l $ZATODIR/server1/pickup-dir
 
